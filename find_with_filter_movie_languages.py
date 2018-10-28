0fc630c4641130bd738802b8a9f435747eb6a0cf
Diego <diego.delpozo@gmail.com>
2018-10-28 15:46:22 +0000

    Experimenting with find and filters, vs match aggregation

---
+++ find_with_filter_movie_languages.py
@@ -0,0 +1,11 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+from IPython.display import clear_output
+
+client = MongoClient("connectionString")
+
+filter = {'language': 'Korean, English'}
+
+clear_output()
+pprint.pprint(list(client.mflix.movies_initial.find(filter)))
