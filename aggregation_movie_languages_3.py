dd2778494eeb76c0f81b5bd33fde47388193ea67
Diego <diego.delpozo@gmail.com>
2018-10-28 15:23:48 +0000

    Simplifying our movie language aggregation using the unique stage "sortByCount"

---
+++ aggregation_movie_languages_3.py
@@ -0,0 +1,15 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+from IPython.display import clear_output
+
+client = MongoClient("connectionString")
+
+pipeline = [
+    {
+        '$sortByCount': "$language"
+    }
+]
+
+clear_output()
+pprint.pprint(list(client.mflix.movies_initial.aggregate(pipeline)))
