98c7afe0ce82a54f6df373433211e9aa1561bb46
Diego <diego.delpozo@gmail.com>
2018-10-28 15:43:06 +0000

    Using a simple match aggregation of movies by language combination

---
+++ aggregation_movie_languages_5.py
@@ -0,0 +1,15 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+from IPython.display import clear_output
+
+client = MongoClient("connectionString")
+
+pipeline = [
+    {
+        '$match': {'language': 'Korean, English'}
+    }
+]
+
+clear_output()
+pprint.pprint(list(client.mflix.movies_initial.aggregate(pipeline)))
