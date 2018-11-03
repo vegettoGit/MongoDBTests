3aedba1718bde0b576a504f1d98317ace127ebb0
Diego <diego.delpozo@gmail.com>
2018-11-03 17:43:20 +0000

    Adding a sample of using the operator "cond" and "dateFromString" to our projection query

---
+++ projecting_movies_2.py
@@ -0,0 +1,55 @@
+# Coursera Introduction to MongoDB
+
+import pymongo
+from pymongo import MongoClient
+import pprint
+from IPython.display import clear_output
+
+client = MongoClient("connectionString")
+
+pipeline = [
+    {
+        '$limit': 100
+    },
+    {
+        '$project': {
+            'title': 1,
+            'year': 1,
+            'directors': {'$split': ["$director", ", "]},
+            'actors': {'$split': ["$cast", ", "]},
+            'writers': {'$split': ["$writer", ", "]},
+            'genres': {'$split': ["$genre", ", "]},
+            'languages': {'$split': ["$language", ", "]},
+            'countries': {'$split': ["$country", ", "]},
+            'plot': 1,
+            'fullPlot': "$fullplot",
+            'rated': "$rating",
+            'released': {
+                '$cond': {
+                    'if': {'$ne': ["$released", ""]},
+                    'then': {
+                        '$dateFromString': {
+                            'dateString': "$released"
+                        }
+                    },
+                    'else': ""}},
+            'runtime': 1,
+            'poster': 1,
+            'imdb': {
+                'id': "$imdbID",
+                'rating': "$imdbRating",
+                'votes': "$imdbVotes"
+                },
+            'metacritic': 1,
+            'awards': 1,
+            'type': 1,
+            'lastUpdated': "$lastupdated"
+        }
+    },
+    {
+        '$out': "movies_scratch"
+    }
+]
+
+clear_output()
+pprint.pprint(list(client.mflix.movies_initial.aggregate(pipeline)))
